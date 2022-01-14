ef69e206bfc3da192e9f350d328ea0252211ce79
carron10 <carronmuleya10@gmail.com>
2022-01-14 11:43:51 +0200

    Added R file for calculating t-test

---
+++ Assignment 2.R
@@ -0,0 +1,7 @@
+
+Baseline=c(210,180,195,220,231,199,224)
+Program.End=c(193,186,186,223,220,183,233)
+my_data <- data.frame(Baseline,Program.End)
+print(my_data)
+res <- t.test(Baseline, Program.End, paired = TRUE)
+
